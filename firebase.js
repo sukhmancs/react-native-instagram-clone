b5969e54d70f0a2502882e70a8b5f9674b6d1a1b
racoot <sukhmanghum66778899@gmail.com>
2024-04-16 10:24:44 -0400

    first commit

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5j4xfCPi00WsS0DYxm-o4JTH3swjTXZo",
-  authDomain: "react-native-ig-clone-7e287.firebaseapp.com",
-  projectId: "react-native-ig-clone-7e287",
-  storageBucket: "react-native-ig-clone-7e287.appspot.com",
-  messagingSenderId: "692960787665",
-  appId: "1:692960787665:web:08131273ae129d1d12d9f0"
+  apiKey: "API_KEY",
+  authDomain: "DOMAIN_NAME",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 }
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
